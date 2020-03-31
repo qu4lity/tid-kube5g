03b32e6f78bb73fafed36ab7dd74c3f2481ecf9b
albertoflorez <alberto.florezpages@telefonica.com>
2020-03-31 17:41:06 +0200

    adjust opennebula vm template to deploy correctly k8s

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,13 @@
 provider "opennebula" {
         endpoint = "http://192.168.83.20:2633/RPC2"
-        username = "xxxxxxxxxxxxx"
-        password = "yyyyyyyyyyyyy"
+        username = "oneadmin"
+        password = "XXXXXX"
 }
 
 data "template_file" "one-kube-template" {
         template = "${file("opennebula_kubernetes_template.tmpl")}"
 }
+
 
 resource "opennebula_template" "one-kube-template" {
         name = "terraform-kubernetes-template"
@@ -14,11 +15,11 @@ resource "opennebula_template" "one-kube-template" {
         permissions = "600"
 }
 
+
 resource "opennebula_virtual_machine" "kube-node" {
         name = "tf-kube-node${count.index}"
         template_id = "${opennebula_template.one-kube-template.id}"
         permissions = "600"
-
         # number of cluster nodes
         count = 3
 }
@@ -30,5 +31,6 @@ output "kube-node-vm_id" {
 output "kube-node-vm_ip" {
         value = "${opennebula_virtual_machine.kube-node.*.ip}"
 }
+
 
 
