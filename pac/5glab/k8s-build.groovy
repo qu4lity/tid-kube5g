cfc3f3c6acc1b6bd7c3e0a573281dc674adef9f8
Jose Nuñez <jonunyez@gmail.com>
2020-07-13 18:22:50 +0200

    deploy k8s aut using nuc5.lab

--- pac/5glab/k8s-build.groovy
+++ pac/5glab/k8s-build.groovy
@@ -1,7 +1,13 @@
+def awsCredentials = [[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: '047a2523-0a76-477f-a24d-17efc60b6a82', accessKeyVariable: 'AWS_ACCESS_KEY_ID', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]
 pipeline {
     agent { node {label 'prod-nc-slave-02'}  }
     options {
         disableConcurrentBuilds()
+        withCredentials(awsCredentials)
+    }
+    environment {
+        MESSAGE   = 'k8s-deployed'
+        QUEUE_URL = 'https://sqs.eu-west-3.amazonaws.com/709233559969/5GnowSQS'
     }
     stages {
         stage ('Provision K8S cluster on baremetal'){
@@ -14,10 +20,25 @@ pipeline {
                          -v $(pwd)/ansible:/srv                             \
                          -v $(pwd)/pac/5glab:/5glab                         \
                          -v $(pwd)/pac/5glab/ssh_config:/etc/ssh/ssh_config \
+                         -v $(pwd)/pac/5glab/ssh_key:/etc/ssh/ssh_key       \
                          --net=host                                         \
                          dockerhub.hi.inet/5ghacking/ansible:2.9.7-2        \
                          -i /5glab/inventory                                \
                          bootstrap.yml
+                       '''
+                }
+            }
+        }
+        stage ('send event to run one-click demo'){
+            steps {
+                dir ("${WORKSPACE}") {
+                    sh '''
+                       export AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}"\
+                       export AWS_DEFAULT_REGION=eu-west-3                    \
+                       export AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}"        \
+                       aws sqs send-message                                   \
+                         --queue-url $QUEUE_URL                               \
+                         --message-body $MESSAGE
                        '''
                 }
             }
